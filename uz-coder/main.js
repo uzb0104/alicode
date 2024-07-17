eb804e5b18cf52cef3da26bcaa40cffcfb91ef8c
Omonov Abbosbek <abbosbekomonov04@gmail.com>
2024-07-17 18:21:40 +0500

    Add files via upload

---
+++ uz-coder/main.js
@@ -0,0 +1,63 @@
+// date.setTime(2015);
+// console.log(setTime());
+
+// let date = new Date();
+// date.setFullYear(2015, 7, 15);
+// let ms = date.getTime();
+// let res = ms / 60000;
+// console.log(date, res);
+
+// let startDate = new Date("2022-02-12");
+
+// let endDate = new Date("2120-02-12");
+
+// let plusDate = endDate + startDate;
+// console.log(plusDate);
+
+// console.log(plusDate.getTime());
+// console.log(plusDate.getFullYear());
+// console.log(plusDate.getDate());
+// console.log(plusDate.getDay());
+// console.log(plusDate.getMonth());
+// console.log(plusDate.getHours());
+// console.log(plusDate.getMinutes());
+// console.log(plusDate.getSeconds());
+// console.log(plusDate.getMilliseconds());
+
+let d = new Date("2022-02-12");
+console.log(d);
+console.log(d.getFullYear());
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getMonth());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getMilliseconds());
+console.log(d.getTime());
+
+let h = new Date("2120-02-12");
+console.log(h);
+console.log(h.getFullYear());
+console.log(h.getDate());
+console.log(h.getDay());
+console.log(h.getMonth());
+console.log(h.getHours());
+console.log(h.getMinutes());
+console.log(h.getSeconds());
+console.log(h.getMilliseconds());
+console.log(h.getTime());
+
+// let text = prompt("Salom 12-02-2120, 12-02-2020");
+// let d = text;
+// console.log(d);
+
+// console.log(d.getTime());
+// console.log(d.getFullYear());
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getMonth());
+// console.log(d.getHours());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
